b2e8e737424def9856d9f7d30ca7ddbfc02517b6
VladDer27 <cadoni827@gmail.com>
2024-12-12 19:40:00 +0300

    Deleted unnecessary import

--- src/main/java/com/vkr/user_service/entity/user/User.java
+++ src/main/java/com/vkr/user_service/entity/user/User.java
@@ -1,6 +1,5 @@
 package com.vkr.user_service.entity.user;
 
-import com.vkr.user_service.entity.avatar.UserProfilePic;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
@@ -9,7 +8,6 @@ import org.hibernate.annotations.UuidGenerator;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 
 import java.time.LocalDateTime;
 import java.util.Collection;
