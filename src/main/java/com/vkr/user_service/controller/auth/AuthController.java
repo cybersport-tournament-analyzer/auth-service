06b3bdec73c52eed395fa23d2aab4c50a4c71180
Ulquiorra <rodiontrig@gmail.com>
2024-12-15 19:30:23 +0300

    add Feign Client

--- src/main/java/com/vkr/user_service/controller/auth/AuthController.java
+++ src/main/java/com/vkr/user_service/controller/auth/AuthController.java
@@ -1,31 +1,45 @@
 package com.vkr.user_service.controller.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.vkr.user_service.dto.response.ResponseDto;
 import com.vkr.user_service.service.auth.AuthService;
+import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:4200/")
 @Tag(name = "Auth Controller")
 public class AuthController {
 
     private final AuthService service;
+//    @JsonProperty("openIdUrl")
+//    String openIdUrl = "https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0"
+//            + "&openid.mode=checkid_setup"
+//            + "&openid.return_to=http://localhost:4200/"
+//            + "&openid.realm=http://localhost:4200/"
+//            + "&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select"
+//            + "&openid.identity=http://specs.openid.net/auth/2.0/identifier_select";
 
     @GetMapping("/login")
     public ResponseEntity<Void> redirectToSteam() {
+//            Map<String, String> response = new HashMap<>();
+//            response.put("openIdUrl", openIdUrl);
+//            return ResponseEntity.ok(response);
         String openIdUrl = "https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0"
                 + "&openid.mode=checkid_setup"
                 + "&openid.return_to=http://localhost:8080/auth/login/redirect"
--- src/main/java/com/vkr/user_service/controller/user/UserController.java
+++ src/main/java/com/vkr/user_service/controller/user/UserController.java
@@ -1,19 +1,25 @@
 package com.vkr.user_service.controller.user;
 
+import com.vkr.user_service.client.MatchmakingServiceClient;
+import com.vkr.user_service.dto.server.Server;
 import com.vkr.user_service.dto.user.UserDto;
 import com.vkr.user_service.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-
+    private final MatchmakingServiceClient matchmakingServiceClient;
     private final UserService userService;
 
     @GetMapping
@@ -42,5 +48,12 @@ public class UserController {
     @Operation(summary = "Delete user")
     public void deleteUser(@PathVariable String username) {
         userService.deleteUser(username);
+    }
+
+
+
+    @GetMapping("/call-matchmaking-service")
+    public Page<Server> callRemoteService(Pageable pageable) throws IOException, InterruptedException {
+        return matchmakingServiceClient.findAll(pageable);
     }
 }
