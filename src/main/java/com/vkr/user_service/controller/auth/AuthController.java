40c6c30f62573ff0de16b2ffd12fc5cd00b02bc0
Ulquiorra <rodiontrig@gmail.com>
2025-02-08 21:30:37 +0300

    fix oauth

--- src/main/java/com/vkr/user_service/controller/auth/AuthController.java
+++ src/main/java/com/vkr/user_service/controller/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.vkr.user_service.controller.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.vkr.user_service.dto.response.ResponseDto;
 import com.vkr.user_service.service.auth.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,37 +13,40 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200/")
 @Tag(name = "Auth Controller")
 public class AuthController {
 
     private final AuthService service;
-//    @JsonProperty("openIdUrl")
-//    String openIdUrl = "https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0"
-//            + "&openid.mode=checkid_setup"
-//            + "&openid.return_to=http://localhost:4200/"
-//            + "&openid.realm=http://localhost:4200/"
-//            + "&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select"
-//            + "&openid.identity=http://specs.openid.net/auth/2.0/identifier_select";
-
-    @GetMapping("/login")
-    public ResponseEntity<String> redirectToSteam() {
-//            Map<String, String> response = new HashMap<>();
-//            response.put("openIdUrl", openIdUrl);
-//            return ResponseEntity.ok(response);
-        String openIdUrl = "https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0"
-                + "&openid.mode=checkid_setup"
+    @JsonProperty("openIdUrl")
+    String openIdUrl = "https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0"
+            + "&openid.mode=checkid_setup"
                 + "&openid.return_to=http://localhost:8080/auth/login/redirect"
                 + "&openid.realm=http://localhost:8080"
-                + "&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select"
-                + "&openid.identity=http://specs.openid.net/auth/2.0/identifier_select";
-        return ResponseEntity.ok(openIdUrl);
+            + "&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select"
+            + "&openid.identity=http://specs.openid.net/auth/2.0/identifier_select";
+
+    @GetMapping("/login")
+    public ResponseEntity<Map<String, String>> redirectToSteam() {
+        Map<String, String> response = new HashMap<>();
+        response.put("openIdUrl", openIdUrl);
+        return ResponseEntity.ok(response);
+//        String openIdUrl = "https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0"
+//                + "&openid.mode=checkid_setup"
+//                + "&openid.return_to=http://localhost:8080/auth/login/redirect"
+//                + "&openid.realm=http://localhost:8080"
+//                + "&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select"
+//                + "&openid.identity=http://specs.openid.net/auth/2.0/identifier_select";
+//        return ResponseEntity.ok(new HashMap<String, String>() {'openIdUrl':openIdUrl});
+//        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(openIdUrl)).build();
+
     }
 
 
@@ -50,8 +54,11 @@ public class AuthController {
     public void loginRedirect(HttpServletResponse response, @RequestParam Map<String, String> allRequestParams) throws IOException, IOException {
         ResponseDto loginResponse = service.login(response, allRequestParams);
 
-        String redirectUrl = "http://localhost:4200/auth.html?accessToken=" + loginResponse.getAccessToken();
+        String redirectUrl = "http://localhost:4200/callback-token?accessToken=" + loginResponse.getAccessToken();
+
         response.sendRedirect(redirectUrl);
+
+
     }
 
     @PostMapping("/refresh")
