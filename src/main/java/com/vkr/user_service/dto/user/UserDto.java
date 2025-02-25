e378c06a651a6ca734b68a57d22337d1edbc0776
VladDer27 <cadoni827@gmail.com>
2025-02-25 16:22:56 +0300

    Change UserDto

--- src/main/java/com/vkr/user_service/dto/user/UserDto.java
+++ src/main/java/com/vkr/user_service/dto/user/UserDto.java
@@ -1,18 +1,11 @@
 package com.vkr.user_service.dto.user;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.vkr.user_service.entity.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -26,8 +19,5 @@ public class UserDto {
     private Long ratingElo;
     private String avatarImageLink;
     private String steamProfileLink;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    private LocalDateTime createdAt;
     private Role role;
 }
