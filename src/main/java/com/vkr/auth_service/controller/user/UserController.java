9e54b48bd9ecdb0dc1ecfd28e130a204b7af90c1
yofujitsu <yofujitsuuu@gmail.com>
2025-03-09 15:59:42 +0300

    now user service is auth service 2

--- src/main/java/com/vkr/auth_service/controller/user/UserController.java
+++ src/main/java/com/vkr/auth_service/controller/user/UserController.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
