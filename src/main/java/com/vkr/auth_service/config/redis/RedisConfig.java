5f53f29a289a1a954be3d09fb2392b8b4d8f6dc7
yofujitsu <yofujitsuuu@gmail.com>
2025-05-01 12:20:39 +0300

    редис с паролем

--- src/main/java/com/vkr/auth_service/config/redis/RedisConfig.java
+++ src/main/java/com/vkr/auth_service/config/redis/RedisConfig.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RefreshScope
 @Configuration
@@ -13,12 +18,17 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
     private String host;
+
     @Value("${spring.data.redis.port}")
     private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
 
     @Bean
     public JedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setPassword(RedisPassword.of(password));
         return new JedisConnectionFactory(config);
     }
 }
